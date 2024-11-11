3b40cca6e3d2da440cdd50d57836b3cb2d1e61b6
Cameron Preasmyer <spc6uz@virginia.edu>
2024-11-10 21:57:37 -0500

    Update setup.py

--- M09_Package/setup.py
+++ M09_Package/setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='Demo',
+    name='my_package',
     version='0.1',
-    description='A simple demonstration package with booklover code',
-    url='http://github.com/ontoligent/m09-demo',
+    packages=find_packages(),
+    install_requires=[],
     author='Cameron Preasmyer',
     author_email='spc6uz@virginia.edu',
-    license='MIT',
-    packages=['M09-Package']
+    description='A simple package for book lovers',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://https://github.com/Preasmyer/M09-Packages',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
